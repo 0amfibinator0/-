281099065e986bb968106a99e5ba59fb36675bb6
0amfibinator0 <00amfibiantor00@mail.ru>
2021-11-21 19:38:46 +0300

    сделал карусель-слайдер

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import modal from './modules/modal';
 import accordeon from './modules/accordeon';
 import scrollUp from './modules/scrollUp';
 import mainSlider from './modules/mainSlider';
+import slider from './modules/slider';
 
 modal();
 accordeon();
 scrollUp();
 mainSlider();
+slider();
