1286e98eb02c08239a21d12bbb3169388ccaaa3c
0amfibinator0 <00amfibiantor00@mail.ru>
2021-11-22 22:55:56 +0300

    плавные якори

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import scrollUp from './modules/scrollUp';
 import mainSlider from './modules/mainSlider';
 import carousel from './modules/carousel';
 import sending from './modules/sending';
+import links from './modules/links';
 
 modal();
 accordeon();
@@ -12,4 +13,5 @@ mainSlider();
 sending({
     formId: 'form'
 });
+links();
 carousel.init();
