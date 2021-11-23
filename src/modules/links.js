b87290bbaa834ae6f263b7f5817938c3e127b94f
0amfibinator0 <00amfibiantor00@mail.ru>
2021-11-23 21:08:05 +0300

    сделал плавные якори

--- src/modules/links.js
+++ src/modules/links.js
@@ -1,18 +1,18 @@
 const links = () => {
     const anchors = document.querySelectorAll('a[href*="#"]');
 
-    for (let anchor of anchors) {
-        anchor.addEventListener('click', function (e) {
+    anchors.forEach((el) => {
+        el.addEventListener('click', function (e) {
             e.preventDefault();
 
-            const blockID = anchor.getAttribute('href').substr(1);
+            const blockID = el.getAttribute('href').substr(1);
 
             document.getElementById(blockID).scrollIntoView({
                 behavior: 'smooth',
                 block: 'start'
             });
         });
-    }
+    });
 };
 
 export default links;
